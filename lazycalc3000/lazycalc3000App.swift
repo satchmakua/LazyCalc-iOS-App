a7da5a4fb9bdb83238e6653f6fe8292b719fd1d9
Satchel Hamilton <47583278+satchmakua@users.noreply.github.com>
2024-07-30 03:08:26 -0700

    Update

---
+++ lazycalc3000/lazycalc3000App.swift
@@ -0,0 +1,17 @@
+//
+//  lazycalc3000App.swift
+//  lazycalc3000
+//
+//  Created by satchel hamilton on 2/2/24.
+//
+
+import SwiftUI
+
+@main
+struct lazycalc3000App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
